e85deff3b3cde3a934fcb4f3b723310769332707
Edgar de Jesus Mendoza Ortegon <emendozadev@gmail.com>
2022-11-23 10:08:23 -0600

    cambios next-config

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const index = () => {
+  return (
+    <div>
+      <Button variant="primary" size="md">Button</Button>
+    </div>
+  )
+}
+
+export default index
